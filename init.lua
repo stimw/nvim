ca0e387b78e30febe1535c2db84934b9ac963359
stimw <me@stimw.com>
2025-07-11 10:04:27 +0800

    init

---
+++ init.lua
@@ -0,0 +1,2 @@
+-- bootstrap lazy.nvem, LazyVim and your plugins
+require("config.lazy")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+{"rose-pine/neovim"},
+{
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "rose-pine-dawn"
+  }
+}}
